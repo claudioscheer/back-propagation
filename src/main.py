17aa23162fc0c72cb7fc4e7ee88d93e94dca1785
Claudio Scheer <claudioscheer@protonmail.com>
2020-04-08 17:39:16 -0300

    Perform back-propagation operation

--- src/main.py
+++ src/main.py
@@ -3,4 +3,13 @@ import nodes
 node_sum = nodes.Sum(-2, 5)
 node_multiplication = nodes.Multiplication(node_sum.forward(), -4)
 result = node_multiplication.forward()
-print(result)
+
+# derivative from the final result to itself
+# same as ((result + 1e-7) - result) / 1e-7
+# always equal to 1
+global_gradient = 1
+node_multiplication.backward(global_gradient)
+print(node_multiplication.backward_gradient_z)
+node_sum.backward(node_multiplication.backward_gradient_q)
+print(node_sum.backward_gradient_x)
+print(node_sum.backward_gradient_y)
