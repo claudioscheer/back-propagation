a1a2d0fbc4341a1e56fdb92115d89c19b501743a
Claudio Scheer <claudioscheer@protonmail.com>
2020-04-08 17:38:45 -0300

    Add multiplication and sum nodes

--- src/nodes/multiplication.py
+++ src/nodes/multiplication.py
@@ -2,18 +2,26 @@ from . import BaseNode
 import numpy as np
 
 
+def _f(q, z):
+    return q * z
+
+
 class Multiplication(BaseNode):
-    def __init__(self, x, y):
+    def __init__(self, q, z):
         """
-            x * y
+            q * y
         """
         super(Multiplication, self).__init__()
-        self.x = x
-        self.y = y
+        self.q = q
+        self.z = z
 
     def forward(self):
-        self.result = self.x * self.y
-        return self.result
+        self.forward_result = _f(self.q, self.z)
+        return self.forward_result
 
-    def backward(self):
-        pass
+    def backward(self, previous):
+        # store the partial derivative for each input
+        self.local_gradient_q = (_f(self.q + 1e-7, self.z) - self.forward_result) / 1e-7
+        self.local_gradient_z = (_f(self.q, self.z + 1e-7) - self.forward_result) / 1e-7
+        self.backward_gradient_q = previous * self.local_gradient_q
+        self.backward_gradient_z = previous * self.local_gradient_z
--- src/nodes/sum.py
+++ src/nodes/sum.py
@@ -2,6 +2,10 @@ from . import BaseNode
 import numpy as np
 
 
+def _f(x, y):
+    return x + y
+
+
 class Sum(BaseNode):
     def __init__(self, x, y):
         """
@@ -12,8 +16,12 @@ class Sum(BaseNode):
         self.y = y
 
     def forward(self):
-        self.result = self.x + self.y
-        return self.result
+        self.forward_result = _f(self.x, self.y)
+        return self.forward_result
 
-    def backward(self):
-        pass
+    def backward(self, previous):
+        # store the partial derivative for each input
+        self.local_gradient_x = (_f(self.x + 1e-7, self.y) - self.forward_result) / 1e-7
+        self.local_gradient_y = (_f(self.x, self.y + 1e-7) - self.forward_result) / 1e-7
+        self.backward_gradient_x = previous * self.local_gradient_x
+        self.backward_gradient_y = previous * self.local_gradient_y
