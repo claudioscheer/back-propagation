ae5d8381e59ae78df494f2782668c8f5c5b1ebee
Claudio Scheer <claudioscheer@protonmail.com>
2020-04-09 00:50:02 -0300

    Add 'Divide' and 'Exponentiation' nodes

--- src/nodes/__init__.py
+++ src/nodes/__init__.py
@@ -3,4 +3,6 @@
 from .base import BaseNode
 from .multiply import Multiply
 from .add import Add
+from .divide import Divide
+from .exponentiation import Exponentiation
 from .sigmoid import Sigmoid
---
+++ src/nodes/divide.py
@@ -0,0 +1,28 @@
+from . import BaseNode
+import numpy as np
+
+
+def _f(x, y):
+    return x / y
+
+
+class Divide(BaseNode):
+    def __init__(self, x, y):
+        """
+            x / y
+        """
+        super(Divide, self).__init__()
+        self.x = x
+        self.y = y
+
+    def forward(self):
+        self.forward_result = _f(self.x, self.y)
+        return self.forward_result
+
+    def backward(self, previous):
+        # store the partial derivative for each input
+        self.local_gradient_x = (_f(self.x + 1e-7, self.y) - self.forward_result) / 1e-7
+        self.local_gradient_y = (_f(self.x, self.y + 1e-7) - self.forward_result) / 1e-7
+        self.backward_gradient_x = previous * self.local_gradient_x
+        self.backward_gradient_y = previous * self.local_gradient_y
+        return [self.backward_gradient_x, self.backward_gradient_y]
