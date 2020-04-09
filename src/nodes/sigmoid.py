633fbb5cb25197c5afb60ac84338105bd81ee0ca
Claudio Scheer <claudioscheer@protonmail.com>
2020-04-08 23:30:15 -0300

    Add Sigmoid node

---
+++ src/nodes/sigmoid.py
@@ -0,0 +1,25 @@
+from . import BaseNode
+import math
+
+
+def _f(x):
+    return 1 / (1 + math.exp(-x))
+
+
+class Sigmoid(BaseNode):
+    def __init__(self, x):
+        """
+            Sigmoid activation function
+        """
+        super(Sigmoid, self).__init__()
+        self.x = x
+
+    def forward(self):
+        self.forward_result = _f(self.x)
+        return self.forward_result
+
+    def backward(self, previous):
+        self.backward_gradient = (
+            (1 - self.forward_result) * self.forward_result
+        ) * previous
+        return self.backward_gradient
